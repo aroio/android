06afbc79e9fbb4f0928abac10595c54d2791663c
Lennart Egbers <lennartegbers@hotmail.com>
2020-04-03 17:02:46 +0200

    initial android setup

---
+++ app/src/main/java/de/abacuselectronics/aroiorc/MainActivity.kt
@@ -0,0 +1,18 @@
+package de.abacuselectronics.aroiorc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.abacuselectronics.aroiorc.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+        .replace(R.id.container, MainFragment.newInstance())
+        .commitNow()
+    }
+  }
+}
