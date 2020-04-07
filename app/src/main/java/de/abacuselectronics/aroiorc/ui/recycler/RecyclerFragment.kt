64042afba75ff345914f6d9d53477c34555b7e52
Lennart Egbers <lennartegbers@hotmail.com>
2020-04-07 19:22:44 +0200

    provide a layoutManager to be set. Default is LinearLayoutManager

--- app/src/main/java/de/abacuselectronics/aroiorc/ui/recycler/RecyclerFragment.kt
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/recycler/RecyclerFragment.kt
@@ -7,17 +7,26 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import de.abacuselectronics.aroiorc.R
 
+/**
+ * Fragment with a single RecyclerView as content.
+ * */
 abstract class RecyclerFragment : Fragment(R.layout.fragment_recycler) {
 
-  protected lateinit var recyclerView: RecyclerView
+  protected open lateinit var recyclerView: RecyclerView
 
+  protected open var layoutManager: RecyclerView.LayoutManager? = null
+
+  /**
+   * [RecyclerView.Adapter] to use for the RecyclerView.
+   * */
   abstract val adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     recyclerView = view.findViewById(R.id.recycler)
     recyclerView.setHasFixedSize(true)
-    recyclerView.layoutManager = LinearLayoutManager(view.context)
+    recyclerView.layoutManager = layoutManager
+      ?: LinearLayoutManager(view.context)
     recyclerView.adapter = adapter
   }
 }
