fab60783446039b2a7767e4c87a7b9bb8a685b47
Lennart Egbers <lennartegbers@hotmail.com>
2020-04-21 20:00:31 +0200

    * adjusted gitignore * init ConfigFragment * init layout file for ConfigFragment

---
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/detail/config/ConfigFragment.kt
@@ -0,0 +1,8 @@
+package de.abacuselectronics.aroiorc.ui.detail.config
+
+import androidx.fragment.app.Fragment
+import de.abacuselectronics.aroiorc.R
+
+class ConfigFragment: Fragment(R.layout.fragment_detail_config) {
+
+}
