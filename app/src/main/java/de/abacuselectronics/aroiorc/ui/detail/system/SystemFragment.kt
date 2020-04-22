303c3b762158d08c34eef988336368ea55a1d524
Lennart Egbers <lennartegbers@hotmail.com>
2020-04-22 12:25:15 +0200

    * init SystemFragment * init layout

---
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/detail/system/SystemFragment.kt
@@ -0,0 +1,7 @@
+package de.abacuselectronics.aroiorc.ui.detail.system
+
+import androidx.fragment.app.Fragment
+import de.abacuselectronics.aroiorc.R
+
+class SystemFragment: Fragment(R.layout.fragment_detail_system) {
+}
