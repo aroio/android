28e3f6bcbcf24004d9bc38b01695dad2bb267ad3
Lennart Egbers <lennartegbers@hotmail.com>
2020-04-22 20:45:31 +0200

    * added SublabelView and related * layout SystemFragment added * added necessary string resources

---
+++ app/src/main/java/de/abacuselectronics/aroiorc/aroioktx/TextViewKtx.kt
@@ -0,0 +1,11 @@
+package de.abacuselectronics.aroiorc.aroioktx
+
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.M
+import android.widget.TextView
+import androidx.annotation.StyleRes
+
+fun TextView.textAppearance(@StyleRes style: Int) {
+  if (SDK_INT >= M) setTextAppearance(style)
+  else setTextAppearance(this.context, style)
+}
