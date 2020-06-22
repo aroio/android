181654ce9cbd3b80e47fc55d03c0ac938409910b
LennartEgb <lennartegbers@hotmail.com>
2020-06-22 21:59:16 +0200

    Update MainActivity.kt
    reformat

--- app/src/main/java/de/abacuselectronics/aroiorc/ui/start/MainActivity.kt
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/start/MainActivity.kt
@@ -14,68 +14,68 @@ import de.abacuselectronics.aroiorc.viewmodel.start.StartViewModel
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(),
-  LoginFragment.Listener,
-  AroioListFragment.Listener {
-
-  private val viewModel: StartViewModel by viewModels()
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main_activity)
-
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-        .replace(R.id.container, AroioListFragment.newInstance())
-        .commitNow()
-    }
-
-    viewModel.state.observe(this, { state ->
-      when (state) {
-        StartViewModel.State.LOADING -> showProgress()
-        StartViewModel.State.SUCCESS -> startDetailActivity()
-        StartViewModel.State.FAIL    -> displayFailure()
-      }
-    })
-  }
-
-  override fun onAttachFragment(fragment: Fragment) {
-    super.onAttachFragment(fragment)
-
-    when (fragment) {
-      is AroioListFragment -> fragment.listener = this
-      is LoginFragment     -> fragment.listener = this
-    }
-  }
-
-  override fun onCancel() {
-    supportFragmentManager.popBackStack()
-  }
-
-  override fun onLogin(username: String, password: String) {
-    lifecycleScope.launch { viewModel.login(username, password) }
-  }
-
-  override fun onListAroioClicked(aroioIpAddress: String) {
-    viewModel.ipAddress = aroioIpAddress
-    supportFragmentManager.beginTransaction()
-      .setDefaultAnimations()
-      .replace(R.id.container, LoginFragment.newInstance(aroioIpAddress))
-      .addToBackStack(null)
-      .commit()
-  }
-
-  private fun startDetailActivity() {
-    startActivity(DetailActivity.createIntent(this))
-  }
-
-  private fun showProgress() {
-    // TODO - show progress
-    Log.i(javaClass.simpleName, "Show the progressbar")
-  }
-
-  private fun displayFailure() {
-    // TODO - display Failure
-    Log.i(javaClass.simpleName, "Display the failure cause")
-  }
-
+	LoginFragment.Listener,
+	AroioListFragment.Listener {
+	
+	private val viewModel: StartViewModel by viewModels()
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.main_activity)
+		
+		if (savedInstanceState == null) {
+			supportFragmentManager.beginTransaction()
+				.replace(R.id.container, AroioListFragment.newInstance())
+				.commitNow()
+		}
+		
+		viewModel.state.observe(this) { state ->
+			when (state) {
+				StartViewModel.State.Loading -> showProgress()
+				StartViewModel.State.Success -> startDetailActivity()
+				is StartViewModel.State.Fail -> displayFailure(state.exception)
+			}
+		}
+	}
+	
+	override fun onAttachFragment(fragment: Fragment) {
+		super.onAttachFragment(fragment)
+		
+		when (fragment) {
+			is AroioListFragment -> fragment.listener = this
+			is LoginFragment     -> fragment.listener = this
+		}
+	}
+	
+	override fun onCancel() {
+		supportFragmentManager.popBackStack()
+	}
+	
+	override fun onLogin(username: String, password: String) {
+		lifecycleScope.launch { viewModel.login(username, password) }
+	}
+	
+	override fun onListAroioClicked(aroioIpAddress: String) {
+		viewModel.ipAddress = aroioIpAddress
+		supportFragmentManager.beginTransaction()
+			.setDefaultAnimations()
+			.replace(R.id.container, LoginFragment.newInstance(aroioIpAddress))
+			.addToBackStack(null)
+			.commit()
+	}
+	
+	private fun startDetailActivity() {
+		startActivity(DetailActivity.createIntent(this))
+	}
+	
+	private fun showProgress() {
+		// TODO - show progress
+		Log.i(javaClass.simpleName, "Show the progressbar")
+	}
+	
+	private fun displayFailure(exception: Exception) {
+		// TODO - display Failure
+		Log.i(javaClass.simpleName, "Display the failure cause: $exception")
+	}
+	
 }
