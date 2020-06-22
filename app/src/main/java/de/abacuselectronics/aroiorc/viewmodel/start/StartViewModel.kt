a37de008aa22e156513dc0d6c9c0d49515e6c4dd
LennartEgb <lennartegbers@hotmail.com>
2020-06-22 21:59:08 +0200

    Update StartViewModel.kt
    prettify ViewModel

--- app/src/main/java/de/abacuselectronics/aroiorc/viewmodel/start/StartViewModel.kt
+++ app/src/main/java/de/abacuselectronics/aroiorc/viewmodel/start/StartViewModel.kt
@@ -7,33 +7,35 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 
 class StartViewModel : ViewModel() {
-
-  enum class State { LOADING, SUCCESS, FAIL }
-
-  lateinit var ipAddress: String
-
-  private val _state = MutableLiveData<State>()
-  val state: LiveData<State> = _state
-
-  suspend fun login(username: String, password: String) = withContext(IO) {
-    if (::ipAddress.isInitialized) {
-      _state.postValue(State.LOADING)
-    } else {
-      _state.postValue(State.FAIL)
-      return@withContext
-    }
-
-    tryLogin(username, password, ipAddress)
-  }
-
-  private suspend fun tryLogin(
-    username: String,
-    password: String,
-    ipAddress: String
-  ) {
-    // TODO - Implement the login process. Currently only set state to success
-    //        for debugging purposes.
-    _state.postValue(State.SUCCESS)
-  }
-
+	
+	sealed class State {
+		object Loading : State()
+		object Success : State()
+		class Fail(val exception: Exception) : State()
+	}
+	
+	lateinit var ipAddress: String
+	
+	private val _state = MutableLiveData<State>()
+	val state: LiveData<State> = _state
+	
+	suspend fun login(username: String, password: String) = withContext(IO) {
+		try {
+			_state.postValue(State.Loading)
+			tryLogin(username, password, ipAddress)
+		} catch (uninitialized: UninitializedPropertyAccessException) {
+			_state.postValue(State.Fail(uninitialized))
+		}
+	}
+	
+	private suspend fun tryLogin(
+		username: String,
+		password: String,
+		ipAddress: String
+	) {
+		// TODO - Implement the login process. Currently only set state to success
+		//        for debugging purposes.
+		_state.postValue(State.Success)
+	}
+	
 }
