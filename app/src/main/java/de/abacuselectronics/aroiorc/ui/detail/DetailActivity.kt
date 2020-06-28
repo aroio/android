54bf3da7f6f3be078564143fc02a68b59b1388e3
LennartEgb <lennartegbers@hotmail.com>
2020-06-28 14:30:06 +0200

    Update DetailActivity.kt
    * reformat

--- app/src/main/java/de/abacuselectronics/aroiorc/ui/detail/DetailActivity.kt
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/detail/DetailActivity.kt
@@ -10,45 +10,43 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import de.abacuselectronics.aroiorc.R
 import de.abacuselectronics.aroiorc.ui.detail.config.ConfigFragment
 import de.abacuselectronics.aroiorc.ui.detail.system.SystemFragment
-import de.abacuselectronics.aroiorc.ui.start.AroioListFragment
 
 class DetailActivity : AppCompatActivity(),
-  BottomNavigationView.OnNavigationItemSelectedListener {
-
-  private lateinit var viewPager: ViewPager2
-  private lateinit var bottomNav: BottomNavigationView
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_detail)
-
-    viewPager = findViewById(R.id.detail_viewPager)
-    viewPager.isUserInputEnabled = false
-    viewPager.adapter = DetailFragmentPagerAdapter(
-      activity = this,
-      fragments = listOf(
-        ConfigFragment.newInstance(),
-        SystemFragment.newInstance(),
-        AroioListFragment.newInstance()
-      )
-    )
-
-    bottomNav = findViewById(R.id.detail_bottom_navigation)
-    bottomNav.setOnNavigationItemSelectedListener(this)
-  }
-
-  override fun onNavigationItemSelected(item: MenuItem): Boolean {
-    when (item.itemId) {
-      R.id.menu_detail_nav_config    -> viewPager.currentItem = 0
-      R.id.menu_detail_nav_system    -> viewPager.currentItem = 1
-      R.id.menu_detail_nav_convolver -> viewPager.currentItem = 2
-      R.id.menu_detail_nav_audio     -> viewPager.currentItem = 3
-    }
-    return true
-  }
-
-  companion object {
-    fun createIntent(context: Context): Intent =
-      Intent(context, DetailActivity::class.java)
-  }
+	BottomNavigationView.OnNavigationItemSelectedListener {
+	
+	private lateinit var viewPager: ViewPager2
+	private lateinit var bottomNav: BottomNavigationView
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_detail)
+		
+		viewPager = findViewById(R.id.detail_viewPager)
+		viewPager.isUserInputEnabled = false
+		viewPager.adapter = DetailFragmentPagerAdapter(
+			activity = this,
+			fragments = listOf(
+				ConfigFragment.newInstance(),
+				SystemFragment.newInstance()
+			)
+		)
+		
+		bottomNav = findViewById(R.id.detail_bottom_navigation)
+		bottomNav.setOnNavigationItemSelectedListener(this)
+	}
+	
+	override fun onNavigationItemSelected(item: MenuItem): Boolean {
+		when (item.itemId) {
+			R.id.menu_detail_nav_config    -> viewPager.currentItem = 0
+			R.id.menu_detail_nav_system    -> viewPager.currentItem = 1
+			R.id.menu_detail_nav_convolver -> viewPager.currentItem = 2
+			R.id.menu_detail_nav_audio     -> viewPager.currentItem = 3
+		}
+		return true
+	}
+	
+	companion object {
+		fun createIntent(context: Context): Intent =
+			Intent(context, DetailActivity::class.java)
+	}
 }
