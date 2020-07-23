137f48a4ea9de7684e358e292f14af1a1bd328d1
LennartEgb <lennartegbers@hotmail.com>
2020-07-23 16:16:58 +0200

    Update MainActivity.kt
    * removed ViewModel controlling everything

--- app/src/main/java/de/abacuselectronics/aroiorc/ui/start/MainActivity.kt
+++ app/src/main/java/de/abacuselectronics/aroiorc/ui/start/MainActivity.kt
@@ -1,21 +1,16 @@
 package de.abacuselectronics.aroiorc.ui.start
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.observe
 import de.abacuselectronics.aroiorc.R
 import de.abacuselectronics.aroiorc.extensions.setDefaultAnimations
-import de.abacuselectronics.aroiorc.ui.detail.DetailActivity
-import de.abacuselectronics.aroiorc.viewmodel.start.StartViewModel
+import de.abacuselectronics.aroiorc.ui.list.AroioListFragment
+import de.abacuselectronics.aroiorc.ui.list.LoginFragment
 
 class MainActivity : AppCompatActivity(),
 	LoginFragment.Listener,
 	AroioListFragment.Listener {
-	
-	private val viewModel: StartViewModel by viewModels()
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -25,14 +20,6 @@ class MainActivity : AppCompatActivity(),
 			supportFragmentManager.beginTransaction()
 				.replace(R.id.container, AroioListFragment.newInstance())
 				.commitNow()
-		}
-		
-		viewModel.state.observe(this) { state ->
-			when (state) {
-				StartViewModel.State.Loading -> showProgress()
-				StartViewModel.State.Success -> startDetailActivity()
-				is StartViewModel.State.Fail -> displayFailure(state.exception)
-			}
 		}
 	}
 	
@@ -49,31 +36,12 @@ class MainActivity : AppCompatActivity(),
 		supportFragmentManager.popBackStack()
 	}
 	
-	override fun onLogin(username: String, password: String) {
-		viewModel.login(username, password)
-	}
-	
 	override fun onListAroioClicked(aroioIpAddress: String) {
-		viewModel.ipAddress = aroioIpAddress
 		supportFragmentManager.beginTransaction()
 			.setDefaultAnimations()
 			.replace(R.id.container, LoginFragment.newInstance(aroioIpAddress))
 			.addToBackStack(null)
 			.commit()
-	}
-	
-	private fun startDetailActivity() {
-		startActivity(DetailActivity.createIntent(this))
-	}
-	
-	private fun showProgress() {
-		// TODO - show progress
-		Log.i(javaClass.simpleName, "Show the progressbar")
-	}
-	
-	private fun displayFailure(exception: Exception) {
-		// TODO - display Failure
-		Log.i(javaClass.simpleName, "Display the failure cause: $exception")
 	}
 	
 }
