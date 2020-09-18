c29a0292cccc69902c79d735abfdf9dc18932e03
LennartEgb <lennartegbers@hotmail.com>
2020-09-18 21:01:13 +0200

    Client now usinng retrofit internally

--- network/src/main/java/de/abacus/aroio/network/Client.kt
+++ network/src/main/java/de/abacus/aroio/network/Client.kt
@@ -1,4 +1,30 @@
 package de.abacus.aroio.network
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 object Client {
+	
+	@Volatile
+	private var retrofit: Retrofit? = null
+	private var baseUrl: String? = null
+	
+	internal fun getRetrofit(aroioIpAddress: String): Retrofit {
+		if (this.baseUrl == aroioIpAddress) return requireNotNull(retrofit)
+		this.baseUrl = aroioIpAddress
+		return createRetrofit(aroioIpAddress)
+	}
+	
+	@OptIn(ExperimentalSerializationApi::class)
+	private fun createRetrofit(baseUrl: String): Retrofit {
+		val contentType = "application/json".toMediaType()
+		return Retrofit.Builder()
+			.baseUrl(baseUrl)
+			.addConverterFactory(Json.asConverterFactory(contentType = contentType))
+			.build()
+	}
+	
 }
