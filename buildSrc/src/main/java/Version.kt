73a8007347681bfdcd800a52e5a2adc38b7ddf9e
LennartEgb <lennartegbers@hotmail.com>
2020-12-23 14:43:34 +0100

    Added dependencies and versions

---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,16 @@
+object Version {
+
+	object Kotlin {
+		const val std = "1.4.21"
+		const val serialization = "1.0.0-RC"
+	}
+
+	object Test {
+		const val junit5 = "5.7.0"
+	}
+
+	object InstTest {
+		const val android = "1.1.2"
+	}
+
+}
